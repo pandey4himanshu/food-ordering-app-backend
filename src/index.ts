73c8d2c647e3541dec27c9fa67c50bc4aba3130e
Himanshu Pandey <himanshu@Himanshus-MacBook-Air.local>
2025-01-22 18:26:28 +0530

    added restaurant search endpoint

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute"
 
 mongoose
   .connect(process.env.MONGODB_CONNECTION_STRING as string)
@@ -28,6 +29,7 @@ app.get("/health", async (req: Request, res: Response) => {
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
 
 app.listen(7001, () => {
   console.log("server started on localhost:7001");
